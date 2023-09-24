dc3b0bc4edb24b44fe51d1095f3c7699f67f4289
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-09-24 12:43:39 +0200

    Task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,57 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+const createMarkup = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<li class="gallery__item">
+          <div class="gallery__link" href="${original}" data-source="${original}">
+              <img src="${preview}" alt="${description}" class='gallery__image'/>
+          </div>
+      </li>`
+  )
+  .join('');
+
+console.log(createMarkup);
+list.innerHTML = createMarkup;
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionPosition: 'top',
+  captionDelay: 250,
+  sourceAttr: 'data-source',
+});
+
+console.log(lightbox);
+
+// import { galleryItems } from './gallery-items.js';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// const items = galleryItems
+//   .map(({ preview, original, description }) => {
+//     return `<li class="gallery__item">
+//       <a class="gallery__link" href="${original}">
+//         <img
+//           class="gallery__image"
+//           src="${preview}"
+//           alt="${description}"
+//         />
+//       </a>
+//     </li>`;
+//   })
+//   .join('');
+
+// const gallery = document.querySelector('.gallery');
+// gallery.innerHTML = items;
+
+// const lightbox = new SimpleLightbox('.gallery__link', {
+//   captions: true,
+//   captionsData: 'alt',
+//   captionsDelay: 250,
+// });
+
+// console.log(lightbox);
