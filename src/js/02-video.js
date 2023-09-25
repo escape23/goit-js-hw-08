ec0771473e043c8cec9389f8e64547344da74a67
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-09-25 21:40:27 +0200

    Finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const STORAGE_KEY = 'videoplayer - current - time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPayerTime, 1000));
+
+function onPayerTime(currentTimeNow) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(currentTimeNow.seconds));
+}
+
+const timeForPlay = Number(localStorage.getItem(STORAGE_KEY));
+
+player
+  .setCurrentTime(timeForPlay)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+
+// console.log(parseTimePlay.seconds);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  emailarea: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.emailarea.addEventListener('input', throttle(onEmailAreaInput, 500));
+
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+refs.form.addEventListener('input', evt => {
+  let formData = {
+    email: refs.emailarea.value,
+    message: refs.textarea.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onEmailAreaInput(evt) {
+  const email = evt.target.value;
+}
+
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parseMessage = JSON.parse(savedMessage);
+  console.log(parseMessage);
+
+  if (parseMessage) {
+    refs.emailarea.value = parseMessage.email;
+    refs.textarea.value = parseMessage.message;
+  }
+}
