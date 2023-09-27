05fe340a3c09b164a8847cba77e8bb88d4890c1f
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-09-27 12:34:16 +0200

    Update 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -15,7 +15,6 @@ const createMarkup = galleryItems
   )
   .join('');
 
-console.log(createMarkup);
 list.innerHTML = createMarkup;
 
 const lightbox = new SimpleLightbox('.gallery__link', {
@@ -24,34 +23,3 @@ const lightbox = new SimpleLightbox('.gallery__link', {
   captionDelay: 250,
   sourceAttr: 'data-source',
 });
-
-console.log(lightbox);
-
-// import { galleryItems } from './gallery-items.js';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// const items = galleryItems
-//   .map(({ preview, original, description }) => {
-//     return `<li class="gallery__item">
-//       <a class="gallery__link" href="${original}">
-//         <img
-//           class="gallery__image"
-//           src="${preview}"
-//           alt="${description}"
-//         />
-//       </a>
-//     </li>`;
-//   })
-//   .join('');
-
-// const gallery = document.querySelector('.gallery');
-// gallery.innerHTML = items;
-
-// const lightbox = new SimpleLightbox('.gallery__link', {
-//   captions: true,
-//   captionsData: 'alt',
-//   captionsDelay: 250,
-// });
-
-// console.log(lightbox);
